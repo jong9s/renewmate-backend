854a1803c314e15ac4623533bf74ac3db46c19b9
jong9 <jonghunseo@naver.com>
2026-08-13 17:27:15 +0900

    2026-08-13 17:27 initialize project

---
+++ src/main/java/com/renewmate/RenewmateApplication.java
@@ -0,0 +1,13 @@
+package com.renewmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenewmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenewmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renewmate/RenewmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renewmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenewmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
